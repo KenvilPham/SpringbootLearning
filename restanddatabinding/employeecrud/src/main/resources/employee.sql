590762d32b255a2811ab5e3d3208abeabbaca290
kenvilpham <kenvil.pham92@gmail.com>
2021-06-09 05:57:58 +0700

    employee

---
+++ restanddatabinding/employeecrud/src/main/resources/employee.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('kenvil', 'pham', 'kenvil.pham92@gmail.com', '123456789');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Tran Duc', 'Hai', 'hai.tran@gmail.com', '223456789');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Phan Van', 'Hoa', 'hoaphan@gmail.com', '123488789');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Kim Dung', 'Doan', 'kp.doan@gmail.com', '773456789');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Hoai', 'Pham', 'hoaip@gmail.com', '111456789');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Bien', 'Nguyen', 'bien.ng@gmail.com', '123456799');
+INSERT INTO employee(first_name, last_name, email_address, passport_number) VALUES('Hien', 'Hoa', 'hien.hoa@gmail.com', '123466689');
